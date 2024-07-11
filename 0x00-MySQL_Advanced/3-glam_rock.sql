2a8fa9d8f53f631b3d45e22020c83b39caefca84
minasamirr <mina430q@gmail.com>
2024-07-12 01:14:56 +0300

    task

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,7 +3,7 @@
 -- If a band hasn't split, the current year (2022) is used
 -- Only bands with "Glam rock" as their main style are considered
 -- Results are ordered by lifespan in descending order
-SELECT band_name,(IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
